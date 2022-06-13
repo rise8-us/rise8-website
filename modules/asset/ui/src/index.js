8997a1450a8bde59dfbb294a3e764a78e829dd4a
Kira Ivarsson <kivarsson@rise8.us>
2022-06-13 14:48:44 -0400

    Add redirects for footer buttons

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,4 +1,11 @@
 export default () => {
   // Your own project level JS may go here
   console.log('Hello World');
+
+  document.getElementById('customer-button').onclick = function () {
+    location.href = '/contact';
+  };
+  document.getElementById('riser-button').onclick = function () {
+    location.href = '/careers';
+  };
 };
