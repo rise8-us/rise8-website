4b1f150e8e9b31b8521f20c755f1ec40fc5a53f8
Kira Ivarsson <kivarsson@rise8.us>
2022-06-14 17:58:58 -0400

    Add blogs to About Us

--- modules/@apostrophecms/home-page/findPieces.js
+++ modules/@apostrophecms/home-page/findPieces.js
@@ -1,7 +1,8 @@
 const findBlogs = require('../../blog/find');
 
 module.exports = async (self, req, count) => {
-  if (req && req.data && req.data.page && [ '@apostrophecms/home-page' ].includes(req.data.page.type)) {
+  if (req && req.data && req.data.page && [ '@apostrophecms/home-page',
+    'about-page' ].includes(req.data.page.type)) {
     return { blogs: await findBlogs(self, req, count) };
   }
 };
