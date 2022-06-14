50224307c6028b55944c99dd4571dba280319334
Joshua Nathaniel Miller <jmiller@rise8.us>
2022-06-14 14:27:29 -0500

    [feat] Create feature blog in blog index page

--- modules/blog-page/index.js
+++ modules/blog-page/index.js
@@ -3,4 +3,20 @@ module.exports = {
   options: {
     label: 'Blog Page',
   },
+  fields: {
+    add: {
+      _featured: {
+        label: 'Featured Blog',
+        type: 'relationship',
+        withType: 'blog',
+        max: 1,
+      },
+    },
+    group: {
+      basic: {
+        label: 'Basic',
+        fields: [ '_featured' ],
+      },
+    },
+  },
 };
