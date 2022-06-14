a67b9348c28416469f03f742f17bb2cb2e924d30
Kira Ivarsson <kivarsson@rise8.us>
2022-06-14 10:31:57 -0400

    Remove onclick handlers

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,18 +1,4 @@
 export default () => {
   // Your own project level JS may go here
   console.log('Hello World');
-
-  const customerButton = document.getElementById('customer-button');
-  const riserButton = document.getElementById('riser-button');
-
-  if (customerButton) {
-    customerButton.onclick = function () {
-      location.href = '/contact';
-    };
-  }
-  if (riserButton) {
-    riserButton.onclick = function () {
-      location.href = '/careers';
-    };
-  }
 };
