b0b1bc227875bcf24805045a1c45501da23cd74f
Kira Ivarsson <kivarsson@rise8.us>
2022-06-15 12:19:40 -0400

    Update grayscale service logos

--- modules/asset/ui/src/components/WhoWeServe.js
+++ modules/asset/ui/src/components/WhoWeServe.js
@@ -5,17 +5,35 @@ export class WhoWeServe extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-    <div class="secondary">
+    <style>
+    .who-we-serve-wrapper {
+      margin-top: 32px;
+    }
+
+    .service-logo-wrapper {
+      display: flex;
+      justify-content: space-between;
+    }
+    
+    .image-grayscale {
+      filter: grayscale(1);
+    }
+    
+    .image-grayscale:hover {
+      filter: none;
+    }
+    </style>
+    <div class="who-we-serve-wrapper">
       <section-header header="Who We Serve"
                       subheader="Trusted guides with proven success"></section-header>
-      <div class="logo-wrapper">
-        <img src="images/logo-sf.svg" alt="">
-        <img src="images/logo-af.svg" alt="">
-        <img src="images/logo-va.svg" alt="">
-        <img src="images/logo-specops.svg" alt="">
-        <img src="images/logo-sec31.svg" alt="">
-        <img src="images/logo-abms.svg" alt="">
-        <img src="images/logo-km.svg" alt="">
+      <div class="service-logo-wrapper">
+        <img class="image-grayscale" src="images/logo-sf.svg" alt="">
+        <img class="image-grayscale" src="images/logo-af.svg" alt="">
+        <img class="image-grayscale" src="images/logo-va.svg" alt="">
+        <img class="image-grayscale" src="images/logo-specops.svg" alt="">
+        <img class="image-grayscale" src="images/logo-sec31.svg" alt="">
+        <img class="image-grayscale" src="images/logo-abms.svg" alt="">
+        <img class="image-grayscale" src="images/logo-km.svg" alt="">
       </div>
     </div>
     `;
