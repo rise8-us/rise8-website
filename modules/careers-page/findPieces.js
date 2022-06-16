27bb274e35fce01b7519fdd0f08f73a0cc6249d8
Michael Silverman <msilverman@rise8.us>
2022-06-16 14:55:00 -0400

    Add job listings from breezy

--- modules/careers-page/findPieces.js
+++ modules/careers-page/findPieces.js
@@ -1,9 +1,54 @@
 const findTips = require('../tip/find');
 
+const getJobs = async (self) => {
+  console.log('here');
+  const response1 = self.apos.http.get('https://rise8.breezy.hr/json', { headers: { 'Request-Timeout': 5 } });
+
+  console.log('Also Here');
+
+  return response1;
+};
+
 module.exports = async (self, req) => {
   if (req && req.data && req.data.page && [ 'careers-page' ].includes(req.data.page.type)) {
+    const jobs = await getJobs(self).catch(() => null);
     return {
+      jobs: mapJobsByDepartment(jobs),
       tips: await findTips(self, req),
     };
   }
+};
+
+const mapJobsByDepartment = (jobs) => {
+  const jobMap = {};
+
+  jobs.forEach(job => {
+    const department = job.department.replace(' ', '_');
+    job = {
+      ...job,
+      display_location: calcJobDisplayLocation(job.location),
+    };
+    if (jobMap[department]) {
+      const jobArray = jobMap[department];
+      jobArray.push(job);
+      jobMap[department] = jobArray;
+    } else {
+      jobMap[department] = [ job ];
+    }
+  });
+
+  console.log(jobMap);
+
+  return jobMap;
+};
+
+const calcJobDisplayLocation = (location) => {
+  let display = '';
+  if (location.name) {
+    display = location.name;
+    if (location.is_remote) {
+      display += ' or Remote';
+    }
+  }
+  return display || 'Remote';
 };
