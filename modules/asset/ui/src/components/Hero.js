dff1f0cb2735acc9a5ddcd66e2d0b69fb3c8d085
Thomas Reynolds <treynolds@rise8.us>
2022-06-16 21:42:51 -0400

    Add mobile styles for service page

--- modules/asset/ui/src/components/Hero.js
+++ modules/asset/ui/src/components/Hero.js
@@ -13,14 +13,14 @@ export class Hero extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="hero">
+        <img src="/images/background-red-slash-mobile.svg" alt="" class="hero-slash-mobile">
         <div class="left">
           <div class="memo">${this.memo}</div>
           <div class="title">${this.title}</div>
           <div class="description">${this.description}</div>
         </div>
-        <div class="slanted-div"></div>
         <div class="right">
-          <div style="width: 600px"></div>
+          <div style="width: 50%"></div>
         </div>
       </div>
     `;
