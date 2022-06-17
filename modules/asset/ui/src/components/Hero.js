11f0fd3ea5bc4e1a9d479f3e538c5bd2c8cac793
Thomas Reynolds <treynolds@rise8.us>
2022-06-17 15:09:29 -0400

    Add mobile styles for prodacity page

--- modules/asset/ui/src/components/Hero.js
+++ modules/asset/ui/src/components/Hero.js
@@ -1,25 +1,38 @@
 export class Hero extends HTMLElement {
   static get observedAttributes() {
-    return [ 'title', 'memo', 'description', 'image', 'large' ];
+    return [ 'title', 'title-image', 'memo', 'description', 'image', 'large', 'pad-top' ];
   }
 
   constructor() {
     super();
     this.title = this.getAttribute('title');
+    this.titleImage = this.getAttribute('title-image');
     this.memo = this.getAttribute('memo');
     this.description = this.getAttribute('description');
     this.image = this.getAttribute('image');
+    this.caption = this.getAttribute('caption');
     this.large = this.getAttribute('large') !== null;
+    this.padTop = this.getAttribute('pad-top') !== null;
+  }
+
+  isEmpty(field) {
+    return (
+      field === null ||
+        field === 'null' ||
+        field === ''
+    );
   }
 
   connectedCallback() {
     this.innerHTML = `
-      <div class="hero ${this.large ? 'large' : ''}">
+      <div class="hero ${this.large ? 'large' : ''} ${this.description ? 'title-slash' : ''}" style="${this.padTop ? 'padding-top: 40px' : ''}">
         <img src="/images/background-red-slash-mobile.svg" alt="" class="hero-slash-mobile">
         <div class="left">
-          <div class="memo">${this.memo}</div>
-          <div class="title">${this.title}</div>
-          <div class="${this.description !== '' ? 'description' : 'no-description'}">${this.description}</div>
+          <img src="${this.titleImage}" alt="" class="title-image" style="${this.isEmpty(this.titleImage) ? 'display: none' : ''}">
+          <div class="memo" style="${this.isEmpty(this.titleImage) ? '' : 'padding-top: 0'}">${this.memo}</div>
+          <div class="title" style="${this.isEmpty(this.title) ? 'display: none' : ''}">${this.title}</div>
+          <div class="description" style="${this.isEmpty(this.description) ? 'display: none' : ''}">${this.description}</div>
+          <div class="caption" style="${this.isEmpty(this.caption) ? 'display: none' : ''}">${this.caption}</div>
         </div>
         <div class="right">
           <img src="${this.image}" alt="" class="image">
