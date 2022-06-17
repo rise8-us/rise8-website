fec0257923c747ffd0b5f8dfa7746841ddf32f7d
Joshua Nathaniel Miller <jmiller@rise8.us>
2022-06-17 10:32:31 -0500

    fix: Update blog editor rich text widget toolbar and add DM Sans Bold

--- modules/blog/index.js
+++ modules/blog/index.js
@@ -62,11 +62,40 @@ module.exports = {
         options: {
           widgets: {
             '@apostrophecms/rich-text': {
-              toolbar: [ 'styles', 'italic' ],
+              toolbar: [
+                'styles',
+                '|',
+                'bold',
+                'italic',
+                'strike',
+                'link',
+                'horizontalRule',
+                '|',
+                'bulletList',
+                'orderedList',
+                '|',
+                'blockquote',
+                'codeBlock',
+                '|',
+                'undo',
+                'redo',
+              ],
               styles: [
                 {
                   tag: 'p',
                   label: 'Paragraph (P)',
+                },
+                {
+                  tag: 'h2',
+                  label: 'Heading 2 (H2)',
+                },
+                {
+                  tag: 'h3',
+                  label: 'Heading 3 (H3)',
+                },
+                {
+                  tag: 'h4',
+                  label: 'Heading 4 (H4)',
                 },
               ],
             },
