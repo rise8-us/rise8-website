a26f0b164fad5f40a1a035b2856b1f82946dc581
Joshua Nathaniel Miller <jmiller@rise8.us>
2022-06-21 11:50:13 -0500

    fix: Start the loader images closer together
    Co-authored-by: Michael Silverman <msilverman@rise8.us>
    Co-authored-by: Thomas Reynolds <treynolds@rise8.us>

--- modules/asset/ui/src/components/EightLoader.js
+++ modules/asset/ui/src/components/EightLoader.js
@@ -6,19 +6,21 @@ export class EightLoader extends HTMLElement {
         ? '../'
         : '';
     setTimeout(function () {
-      document.getElementById('website-content').classList.remove('display-after-loading-screen');
-      document.getElementById('loader-wrapper').classList.add('loaded');
+      // document.getElementById('website-content').classList.remove('display-after-loading-screen');
+      // document.getElementById('loader-wrapper').classList.add('loaded');
     }, 1000);
   }
 
   connectedCallback() {
     this.innerHTML = `
       <div id="loader-wrapper" class="loader-wrapper">
-        <div style="width: 50%">
-          <div class="loader left"><img src="${this.imagePathPrefix}images/logo-8-half-left.svg" alt=""></div>
-        </div>
-        <div style="width: 50%">
-          <div class="loader right"><img src="${this.imagePathPrefix}images/logo-8-half-right.svg" alt=""></div>
+        <div class="loader-container">
+          <div style="width: 50%">
+            <div class="loader left"><img src="${this.imagePathPrefix}images/logo-8-half-left.svg" alt=""></div>
+          </div>
+          <div style="width: 50%">
+            <div class="loader right"><img src="${this.imagePathPrefix}images/logo-8-half-right.svg" alt=""></div>
+          </div>
         </div>
       </div>
     `;
