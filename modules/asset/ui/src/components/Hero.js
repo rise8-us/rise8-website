dcb63cd088274f3d58fb1872febf0e417ecf9b67
Kira Ivarsson <kivarsson@rise8.us>
2022-06-21 10:51:49 -0400

    Fix home and prodacity heros

--- modules/asset/ui/src/components/Hero.js
+++ modules/asset/ui/src/components/Hero.js
@@ -32,7 +32,7 @@ export class Hero extends HTMLElement {
           <div class="memo" style="${Hero.isEmpty(this.titleImage) ? '' : 'padding-top: 20px'} ${Hero.isEmpty(this.memo) ? 'display: none' : ''}">${this.memo}</div>
           <div class="title" style="${Hero.isEmpty(this.title) ? 'display: none' : ''}">${this.title}</div>
           <div class="description" style="${Hero.isEmpty(this.description) ? 'display: none' : ''}">${this.description}</div>
-          <div class="caption ${this.caption}">${this.caption ?? ''}</div>
+          <div class="caption" style="${Hero.isEmpty(this.caption) ? 'display: none' : ''}">${this.caption}</div>
         </div>
         <div class="right">
           <img src="${this.image}" alt="" class="image">
