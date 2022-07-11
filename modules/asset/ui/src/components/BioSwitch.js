3c35f23f6d7fb782a529751be000800439b73418
Kira Ivarsson <kivarsson@rise8.us>
2022-07-11 12:45:39 -0400

    [Bug] Allow user to click anywhere on Riser photos

--- modules/asset/ui/src/components/BioSwitch.js
+++ modules/asset/ui/src/components/BioSwitch.js
@@ -1,6 +1,6 @@
 export class BioSwitch extends HTMLElement {
   static get observedAttributes() {
-    return [ 'image-url', 'department', 'index', 'active' ];
+    return [ 'image-url', 'department', 'index', 'active', 'name', 'role' ];
   }
 
   constructor() {
@@ -9,22 +9,32 @@ export class BioSwitch extends HTMLElement {
     this.department = this.getAttribute('department');
     this.index = this.getAttribute('index');
     this.active = this.getAttribute('active');
+    this.name = this.getAttribute('name');
+    this.role = this.getAttribute('role');
     this.addEventListener('click', (e) => {
       this.onBioClick(e);
     });
   }
 
   onBioClick(e) {
-    const bioPhotos = document.getElementsByClassName("riser-photo");
+    const bioPhotos = document.getElementsByClassName('riser-photo');
     for (let i = 0; i < bioPhotos.length; i++) {
       bioPhotos[i].classList.remove('active');
     }
-    const bios = document.getElementsByClassName("riser-bio-wrapper");
+    const bios = document.getElementsByClassName('riser-bio-wrapper');
     for (let i = 0; i < bios.length; i++) {
       bios[i].classList.remove('active');
     }
-    e.target.classList.add('active');
-    document.getElementById(`${e.target.id.replace('photo', 'bio')}`).classList.add('active');
+
+    let toMakeActive = e.target;
+    if (!e.target.classList.contains('riser-photo')) {
+      const photoContainer = e.path.find(
+        el => el.classList.contains('riser-photo-wrapper'));
+      toMakeActive = photoContainer.getElementsByClassName('riser-photo')[0];
+    }
+    toMakeActive.classList.add('active');
+    document.getElementById(
+      `${toMakeActive.id.replace('photo', 'bio')}`).classList.add('active');
   }
 
   connectedCallback() {
@@ -32,6 +42,8 @@ export class BioSwitch extends HTMLElement {
      <img src=${this.imageUrl} alt="" 
           id="${this.department}-photo-${this.index}" 
           class="riser-photo ${this.department}-riser ${this.active}">
+     <photo-effects name="${this.name}" role="${this.role}"
+                    hover-text="Read Bio"></photo-effects>
     `;
   }
 }
