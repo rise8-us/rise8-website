b375339123814829c334c1dbc8298505e65ca73a
Joshua Nathaniel Miller <jmiller@rise8.us>
2022-07-15 21:48:20 -0500

    chore: Update tests for flag

--- modules/flag/find.test.js
+++ modules/flag/find.test.js
@@ -20,18 +20,55 @@ const apostrophe = {
   },
 };
 
+const req = {
+  user: {
+    _id: 'user_id',
+  },
+};
+
 describe('find', () => {
-  it('should find by page type', async () => {
+  it('should find flag true when enabled true', async () => {
     const testFlag = {
       title: 'Test Flag',
       slug: 'test-flag',
       enabled: true,
+      usersIds: [],
     };
 
     toArray.mockReturnValueOnce([ testFlag ]);
 
-    const flags = await find(apostrophe, {});
+    const flags = await find(apostrophe, req);
 
     expect(flags).toEqual({ 'test-flag': true });
+  });
+
+  it('should be find flag true when user added and enabled false', async () => {
+    const testFlag = {
+      title: 'Test Flag',
+      slug: 'test-flag',
+      enabled: false,
+      usersIds: [ 'user_id' ],
+    };
+
+    toArray.mockReturnValueOnce([ testFlag ]);
+
+    const flags = await find(apostrophe, req);
+
+    expect(flags).toEqual({ 'test-flag': true });
+  });
+
+  it('should not find when disabled and empty users', async () => {
+    const testFlag = {
+      title: 'Test Flag',
+      slug: 'test-flag',
+      enabled: false,
+      usersIds: [],
+    };
+
+    toArray.mockReturnValueOnce([ testFlag ]);
+
+    const flags = await find(apostrophe, req);
+
+    expect(flags).toEqual({ 'test-flag': false });
   });
 });
